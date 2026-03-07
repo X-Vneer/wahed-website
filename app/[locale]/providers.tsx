2b3434060e09f6118cdd2d7cc4e31368c0c07a53
Moneer .A Mizyed <xv.neer.business@gmail.com>
2026-03-07 23:07:08 +0200

    Add Lenis for smooth scrolling, update package.json and pnpm-lock.yaml, and integrate LenisScroll component in Providers

--- app/[locale]/providers.tsx
+++ app/[locale]/providers.tsx
@@ -1,9 +1,15 @@
 "use client"
 import * as React from "react"
 import { HeroUIProvider } from "@heroui/system"
+import LenisScroll from "@/components/common/lenis-scroll"
 
 function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>
+  return (
+    <HeroUIProvider>
+      {children}
+      <LenisScroll />
+    </HeroUIProvider>
+  )
 }
 
 export default Providers
---
+++ components/common/lenis-scroll.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { ReactLenis } from "lenis/react"
+
+function LenisScroll() {
+  return <ReactLenis root />
+}
+
+export default LenisScroll
