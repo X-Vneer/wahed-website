a8e9055676fca2918062fe848996d05b30c43851
Moneer .A Mizyed <xv.neer.business@gmail.com>
2026-04-14 22:49:49 +0300

    update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
             <div>
               <div className="pt-2 text-center">
                 <h1 className="mb-1 text-3xl font-bold">
-                  404 - Page Not Found
+                  404 - Page Not Found !
                 </h1>
                 <p>The page you are looking for does not exist.</p>
               </div>
